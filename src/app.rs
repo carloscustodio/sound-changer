0db0ca4d01a563e6856bfacfa1df8d195e71788e
carloscustodio <chcustodio97+github.com>
2025-08-17 18:00:04 -0300

    import worked, css did not

--- src/app.rs
+++ src/app.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 use wasm_bindgen::prelude::*;
-use wasm_bindgen_futures::spawn_local;
+use web_sys::Element;
 use yew::prelude::*;
 
 #[wasm_bindgen]
@@ -14,69 +14,21 @@ struct GreetArgs<'a> {
     name: &'a str,
 }
 
+use subwayui::{components::Tile, MetroProvider};
+
 #[function_component(App)]
 pub fn app() -> Html {
-    let greet_input_ref = use_node_ref();
-
-    let name = use_state(|| String::new());
-
-    let greet_msg = use_state(|| String::new());
-    {
-        let greet_msg = greet_msg.clone();
-        let name = name.clone();
-        let name2 = name.clone();
-        use_effect_with(
-            name2,
-            move |_| {
-                spawn_local(async move {
-                    if name.is_empty() {
-                        return;
-                    }
-
-                    let args = serde_wasm_bindgen::to_value(&GreetArgs { name: &*name }).unwrap();
-                    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-                    let new_msg = invoke("greet", args).await.as_string().unwrap();
-                    greet_msg.set(new_msg);
-                });
-
-                || {}
-            },
-        );
-    }
-
-    let greet = {
-        let name = name.clone();
-        let greet_input_ref = greet_input_ref.clone();
-        Callback::from(move |e: SubmitEvent| {
-            e.prevent_default();
-            name.set(
-                greet_input_ref
-                    .cast::<web_sys::HtmlInputElement>()
-                    .unwrap()
-                    .value(),
-            );
-        })
-    };
-
     html! {
-        <main class="container">
-            <h1>{"Welcome to Tauri + Yew"}</h1>
-
-            <div class="row">
-                <a href="https://tauri.app" target="_blank">
-                    <img src="public/tauri.svg" class="logo tauri" alt="Tauri logo"/>
-                </a>
-                <a href="https://yew.rs" target="_blank">
-                    <img src="public/yew.png" class="logo yew" alt="Yew logo"/>
-                </a>
-            </div>
-            <p>{"Click on the Tauri and Yew logos to learn more."}</p>
-
-            <form class="row" onsubmit={greet}>
-                <input id="greet-input" ref={greet_input_ref} placeholder="Enter a name..." />
-                <button type="submit">{"Greet"}</button>
-            </form>
-            <p>{ &*greet_msg }</p>
-        </main>
+        <MetroProvider>
+            <Tile title={Some("Metro Welcome".to_string())} tilt={true} tilt_max={18.0}>
+                { "This is a subwayui tile with tilt enabled." }
+            </Tile>
+            <Tile title={Some("Static SubwayUI Tile".to_string())} tilt={false}>
+                { "This is a static subwayui tile without tilt." }
+            </Tile>
+            <Tile title={Some("Custom Tilt".to_string())} tilt={true} tilt_max={10.0}>
+                { "Another subwayui tile with custom tilt max." }
+            </Tile>
+        </MetroProvider>
     }
 }
